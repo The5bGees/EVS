afd5a1cfe6f5a3d96925df57fc532953e78da90d
Jorge <Jcvarela@live.com>
2017-10-08 19:26:13 -0400

    Starting new github repo

---
+++ modules/list_view/client/config/list_view.client.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Configuring the Articles module
+angular.module('list_view').run(['Menus',
+    function (Menus) {
+        // Add the articles dropdown item
+        Menus.addMenuItem('topbar', {
+            title: 'List View',
+            state: 'list_view',
+            roles: ['user','admin'] //everyone can see it
+        });
+    }
+]);
---
+++ modules/list_view/client/controllers/list_view.client.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create the 'chat' controller
+angular.module('list_view').controller('ListViewController', ['$scope', '$location', 'Authentication', 'Socket',
+    function ($scope, $location, Authentication, Socket) {
+
+    }
+]);
