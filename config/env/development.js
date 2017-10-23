f6444e5027a47d921b1e9036f18d83a77d95ba8f
Jorge <Jcvarela@live.com>
2017-10-23 12:49:12 -0400

    add comments

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 var defaultEnvConfig = require('./default');
 
 module.exports = {
-  db: {
+  db: {                                                     //DON"T MODIFY THE LINE BELOW
     uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
     options: {},
     // Enable mongoose debug mode
