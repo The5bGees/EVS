c2edf623e8bff6efc5bc6d3478ceb9663cb8a642
Jorge <Jcvarela@live.com>
2017-11-05 23:25:49 -0500

    Done for today!

--- modules/list-view/server/models/report.server.model.js
+++ modules/list-view/server/models/report.server.model.js
@@ -34,35 +34,35 @@ let ReportSchema = new Schema({
   //   trim: true,
   //   default: ""
   // },
-  country_of_origin : {
-    type : String,
+  country_of_origin: {
+    type: String,
     trim: true,
     default: ""
   },
-  result : {
-    type : String,
+  result: {
+    type: String,
     trim: true,
-    default : "NA"
+    default: "NA"
   },
-  simplify_pdf : {
-    type : String,
+  simplify_pdf: {
+    type: String,
     trim: true,
-    default : "NA"
+    default: "NA"
   },
-  extended_pdf : {
-    type : String,
+  extended_pdf: {
+    type: String,
     trim: true,
-    default : "NA"
+    default: "NA"
   },
-  oil : {
-    id : {
-      type : Schema.ObjectId,
+  oil: {
+    _id: {
+      type: Schema.ObjectId,
       ref: 'Oil'
     },
     name: {
       type: String,
       trim: true,
-      default : "NA"
+      default: "NA"
     }
     // ,botanical_name : {
     //   type: Schema.botanical_name
@@ -85,5 +85,66 @@ let ReportSchema = new Schema({
 // ReportSchema.pre('findOneAndUpdate', function(next,req,callback){
 //   next();
 // });
+//pre-> is before
+//post-> after
+//this._doc contains
+/***
+ * Schema Save Middleware
+ *
+ * Before saving to the DB add the id to its Oil, to keep the reference
+ */
+ReportSchema.pre('save', function (next, req, callback) {
+  let Oil = mongoose.model('Oil');
+  let self = this._doc;
+
+  Oil.findOne({name: self.oil.name}, function (err, oil) {
+
+    if (!err && oil) {
+      oil.reports.push(self._id);
+
+      Oil.findOneAndUpdate({name: self.oil.name},
+        {reports: oil.reports},
+        function (err) {
+          if (err) {
+            next();
+          }
+          else {
+            self.oil.id = oil._id;
+            next();
+          }
+        });
+    } else {
+      next();
+    }
+  })
+});
+
+//TODO jorge: STOP HERE!!!!
+ReportSchema.pre('findOneAndUpdate', function (next, req, callback) {
+  let Oil = mongoose.model('Oil');
+  let self = this._doc;
+  this._update
+
+  Oil.findOne({name: self.oil.name}, function (err, oil) {
+
+    if (!err && oil) {
+      oil.reports.push(self._id);
+
+      Oil.findOneAndUpdate({name: self.oil.name},
+        {reports: oil.reports},
+        function (err) {
+          if (err) {
+            next();
+          }
+          else {
+            self.oil.id = oil._id;
+            next();
+          }
+        });
+    } else {
+      next();
+    }
+  })
+});
 
 mongoose.model('Report', ReportSchema);
