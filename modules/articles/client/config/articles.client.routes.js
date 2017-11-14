9c5c23eb574a5eea0f3e152921244709fc526785
Zoe <zmongan96@ufl.edu>
2017-11-14 16:07:56 -0500

    subscriber

--- modules/articles/client/config/articles.client.routes.js
+++ modules/articles/client/config/articles.client.routes.js
@@ -20,7 +20,7 @@
         controller: 'ArticlesListController',
         controllerAs: 'vm',
         data: {
-          roles: ['user', 'admin']
+          roles: ['subscriber', 'admin']
         }
       })
       .state('articles.view', {
