bb03803ba7b681f137ff2a2e697ec598fd2d579e
Zoe <zmongan96@ufl.edu>
2017-11-14 14:42:25 -0500

    .

--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -21,6 +21,20 @@ module.exports = function (app) {
       // asynchronously called
     });
   });
+  app.post('/api/users/updatecard', function (request, response) {
+    stripe.customers.update(req.body.id, {
+      source: request.body.token
+    }, function (err, customer) {
+
+    });
+  });
+  app.post('/api/users/cancel', function (request, response) {
+    stripe.subscriptions.del(
+      req.body.stripeSubscription,
+      function (err, customer) {
+      // asynchronously called
+    });
+  });
 
   // Finish by binding the user middleware
   app.param('userId', users.userByID);
