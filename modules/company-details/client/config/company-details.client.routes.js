88c7c8510f4713e503dc531087556527821f2ae1
Daniel <DAScott321@gmail.com>
2017-11-20 13:04:22 -0500

    Added files for company view

---
+++ modules/company-details/client/config/company-details.client.routes.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+// Configure the 'oil details' module routes
+angular.module('oil-details').config(['$stateProvider',
+  function ($stateProvider) {
+    $stateProvider
+      .state('oil-details', {
+        url: '/oil-details',
+        templateUrl: 'modules/oil-details/client/views/oil-details.client.view.html',
+        params:{
+          'oil' : 'empty'
+        }
+      })
+  }
+]);
