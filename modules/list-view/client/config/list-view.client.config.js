a2a82a88ebb78291cb151c7dceaeeee61a35b41c
Jorge <Jcvarela@live.com>
2017-11-29 15:01:14 -0500

    Trying to fix navbar

--- modules/list-view/client/config/list-view.client.config.js
+++ modules/list-view/client/config/list-view.client.config.js
@@ -7,7 +7,7 @@ angular.module('list-view').run(['menuService',
     Menus.addMenuItem('topbar', {
       title: 'EVS Database',
       state: 'list-view'
-      // roles: ['user', 'admin'] //everyone can see it
+      // roles: ['guest','user', 'admin']
     });
   }
 ]);
--- modules/list-view/client/config/list-view.client.routes.js
+++ modules/list-view/client/config/list-view.client.routes.js
@@ -8,23 +8,32 @@ angular.module('list-view').config(['$stateProvider',
         url: '/list-view',
         controller: 'ListViewController',
         templateUrl: 'modules/list-view/client/views/list-view.client.view.html',
-        data: {
-          roles: ['user', 'admin'] // everyone can use this link
-        }
-      })
-      .state('list-view-admin', {
-        url: '/list-view-admin',
-        controller: 'ListViewController',
-        templateUrl: 'modules/list-view/client/views/list-view-admin.client.view.html',
-        data: {
-          roles: ['user', 'admin'] // everyone can use this link
-        }
-      }).state('show-list', {
-      url: '/show-list',
-      templateUrl: 'modules/list-view/client/views/show-list.client.view.html',
-      data: {
-        roles: ['user', 'admin'] // everyone can use this link
-      }
+        // data: {
+        //   roles: ['user']
+        // }
+      // })
+      // .state('list-view',{
+      //   url:'/list-view',
+      //   templateUrl: 'modules/list-view/client/views/list-view.client.view.html',
+      //   data: {
+      //     roles: ['guest']
+      //   }
+      // })
+      // // })
+      // // .state('list-view-admin', {
+      // //   url: '/list-view-admin',
+      // //   controller: 'ListViewController',
+      // //   templateUrl: 'modules/list-view/client/views/list-view-admin.client.view.html',
+      // //   data: {
+      // //     roles: ['user', 'admin'] // everyone can use this link
+      // //   }
+      // })
+      // .state('show-list', {
+      // url: '/show-list',
+      // templateUrl: 'modules/list-view/client/views/show-list.client.view.html',
+      // data: {
+      //   roles: ['user', 'admin'] // everyone can use this link
+      // }
     });
   }
 ]);
