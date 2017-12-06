248cc3da4f7d834b40250d595167d19713ee937c
Jorge <Jcvarela@live.com>
2017-12-06 10:35:22 -0500

    Added the DB

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {                                                     //DON"T MODIFY THE LINE BELOW
-    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || "mongodb://unknown:12345@ds133136.mlab.com:33136/essential_validation", //|| 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
     options: {},
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
