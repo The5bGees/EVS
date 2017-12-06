5b1c48cbe01685268a6b16e7aeb641b293120a5a
Jorge <Jcvarela@live.com>
2017-12-05 20:03:34 -0500

    Merge branch 'D' into list-view

--- modules/oil-details/client/controllers/oil-details.client.controller.js
+++ modules/oil-details/client/controllers/oil-details.client.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Create the 'chat' controller
-angular.module('oil-details').controller('OilDetailsController', ['$scope', '$stateParams', 'Report', '$uibModal', '$location', 'Authentication', 'Socket',
-  function ($scope, $stateParams, Report, $uibModal, $location, Authentication, Socket) {
+angular.module('oil-details').controller('OilDetailsController', ['$scope', '$stateParams', 'Report', '$uibModal', '$state', '$http',
+  function ($scope, $stateParams, Report, $uibModal, $state, $http) {
     $scope.oil = $stateParams.oil;
     $scope.reports = [];
 
@@ -22,19 +22,23 @@ angular.module('oil-details').controller('OilDetailsController', ['$scope', '$st
     $scope.find();
 
     $scope.removeOil = function(){
-      $http({
-        method: 'DELETE',
-        url: '/api/report/icon/',
-        params: {
-          path: fileName
+       let result = confirm("Are you sure you want to delete " + $scope.oil.name + " ?");
+        if(result){
+          $http({
+            method: 'DELETE',
+            url: '/api/oil/',
+            params: {
+              oilId: $scope.oil._id
+            }
+          })
+            .then((res) => {
+              $state.go('list-view');
+            }).catch((err) => {
+            console.log("Error removing oil")
+          });
         }
-      })
-        .then((res) => {
-          console.log(res);
-        }).catch((err) => {
-        console.log(err);
-      });
     };
+
 
     $scope.getDate = function(d){
       let date = new Date(d);
